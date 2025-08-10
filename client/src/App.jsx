b57fa87c20d4788a77e8284b800a70cbb856e1c5
Artternative <kasinat3mjt@gmail.com>
2025-08-10 13:17:40 +0700

    Connect with server prepare for fetch data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function App() {
-  return <div className="App">{/* Start coding here */}</div>;
+export default function App() {
+  useEffect(() => {
+    getResult();
+  }, []);
+  const getResult = async () => {
+    const response = await axios.get("http://localhost:4001/trips");
+    console.log(response.data);
+  };
+
+  return (
+         <div></div>
+  );
 }
-
-export default App;
